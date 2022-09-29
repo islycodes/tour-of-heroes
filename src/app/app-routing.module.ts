160d24c35c67b78443f1373db9756a72f046cb5c
Adrielly Isly <islyg22@gmail.com>
2022-09-29 14:10:11 -0300

    add

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, //this route directs that fully matches the empty path to the route whose path is /dashboard.
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }, // the ':' indicates that :id is a placeholder for a specific hero id.
+  { path: 'heroes', component: HeroesComponent }
+];
+
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
